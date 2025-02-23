11288741011391985cb7153a9726de28bebdcd8d
Harald Wartig <hwartig@gmail.com>
2025-02-23 10:14:10 +0100

    Add basic layout / page setup.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,16 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex min-h-screen w-full flex-col bg-muted/40">
         <main className="flex min-h-screen w-full flex-col ">
-          {children}
+          <div className="flex flex-col sm:gap-4 sm:py-4 sm:p-14">
+            <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+              {/* <Logo/> <Nav/> */}
+            </header>
+            <main className="grid flex-1 items-start gap-2 p-4 sm:px-6 sm:py-0 md:gap-4">
+              {children}
+            </main>
+          </div>
         </main>
       </body>
-    </html>
+    </html >
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <h1>PuppySpa</h1>
+      <Card>
+        <CardHeader>
+          <Link href={"/?date=yesterday"}>
+            <ChevronLeft />
+          </Link>
+          <CardTitle className="flex-10/12 text-center">Waiting List for #today</CardTitle>
+          <Link href={"/?date=tomorrow"}>
+            <ChevronRight />
+          </Link>
+        </CardHeader>
+        <CardContent>
+          {/* Table */}
+        </CardContent>
+        <CardFooter>
+          {/* Form goes here */}
+        </CardFooter>
+      </Card >
     </>
   );
 }
