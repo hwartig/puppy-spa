2f488cc287fdf736e93b62b3534dbae48e454bc1
Harald Wartig <hwartig@gmail.com>
2025-02-23 10:28:04 +0100

    Add placeholder data.

---
+++ lib/placeholder-data.ts
@@ -0,0 +1,7 @@
+export const waitingListEntries = [
+  { number: 1, owner: "Andrea", pet: "Bianca", arrival: "10:01", service: "Haircut" },
+  { number: 2, owner: "Tara", pet: "Lilly", arrival: "10:05", service: "Nails" },
+  { number: 3, owner: "Tara", pet: "Teddy", arrival: "11:10", service: "Nails" },
+  { number: 4, owner: "Harry", pet: "Peppy", arrival: "12:10", service: "Teeth" },
+];
+
